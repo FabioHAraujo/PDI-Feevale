3d75d0e5a650a7dc1b46c17762b8dce5141c388e
Fábio Henrique Araújo <contato@fabioharaujo.com.br>
2024-09-12 21:06:46 +0200

    Update - Logo

--- src/App.vue
+++ src/App.vue
@@ -23,7 +23,7 @@
     -->
     <div class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
       <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-        <img class="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company" />
+        <img class="mx-auto h-10 w-auto" src="https://pocket.fabioharaujo.com.br/api/files/l69dtzzp4ly8n52/vytau34zlnb0vrn/a608824b_c6ad_4fbe_b41f_7b1bbc7cd93e_iAhx8qkA6P.jpg?token=" alt="Morphize Logo" />
         <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign in to your account</h2>
       </div>
   
