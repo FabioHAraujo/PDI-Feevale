fc15f662707482ab40536f2154dc0fd9f1847e14
Fábio Henrique Araújo <contato@fabioharaujo.com.br>
2024-09-12 21:08:10 +0200

    Corrigido template inicial - Próxima etapa será a área de cadastro.

--- src/App.vue
+++ src/App.vue
@@ -24,7 +24,7 @@
     <div class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
       <div class="sm:mx-auto sm:w-full sm:max-w-sm">
         <img class="mx-auto h-10 w-auto" src="https://pocket.fabioharaujo.com.br/api/files/l69dtzzp4ly8n52/vytau34zlnb0vrn/a608824b_c6ad_4fbe_b41f_7b1bbc7cd93e_iAhx8qkA6P.jpg?token=" alt="Morphize Logo" />
-        <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign in to your account</h2>
+        <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Acesse Sua Conta</h2>
       </div>
   
       <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
@@ -38,9 +38,9 @@
   
           <div>
             <div class="flex items-center justify-between">
-              <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
+              <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Senha</label>
               <div class="text-sm">
-                <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
+                <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">Esqueceu sua senha?</a>
               </div>
             </div>
             <div class="mt-2">
@@ -49,14 +49,14 @@
           </div>
   
           <div>
-            <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
+            <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Acesse</button>
           </div>
         </form>
   
         <p class="mt-10 text-center text-sm text-gray-500">
-          Not a member?
+          Ainda não está cadastrado?
           {{ ' ' }}
-          <a href="#" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Start a 14 day free trial</a>
+          <a href="#" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Cadastre-se Clicando Aqui</a>
         </p>
       </div>
     </div>
